89ae9092878e2ee6a9d57352331b46652b23510c
Basti <pfitzseb@physik.hu-berlin.de>
2014-10-20 11:18:48 +0200

    added external fitting routine

---
+++ GUI/fitdata.m
@@ -0,0 +1,48 @@
+function [params, p_err, chisq] = fitdata(model, x, y, err, start_in, fix)
+
+    % model is a cell array from ui.models('key')
+    
+    % fix is a cell array containing variables to be fixed to their values
+    % in start_in
+
+    start = start_in;
+    ubound = model{3};
+    lbound = model{2};
+    fix_ind = [];
+    if nargin == 6 && ~isempty(fix)
+        tmp = func2str(model{1});
+        fix_ind = zeros(length(fix),1);
+        for i = 1:length(fix)
+            fix_ind(i) = find(strcmp(model{4}, fix{i})); % indices of coeffs that are fixed
+            tmp = regexprep(tmp, [fix{i} ','], '', 'once');
+            tmp = regexprep(tmp, ['(?<![\w])' fix{i} '(?![\(\w])'],...
+                            num2str(start_in(fix_ind(i))));
+        end
+        start(fix_ind) = [];
+        ubound(fix_ind) = [];
+        lbound(fix_ind) = [];
+        
+        func = str2func(tmp);
+    else
+        func = model{1};
+    end
+    
+    ft = fittype(func, 'independent', 't');
+    fo = fitoptions('Method', 'NonlinearLeastSquares', 'Lower', lbound,...
+                    'upper', ubound, 'weights', err,...
+                    'StartPoint', start);
+                            
+    [fitobject, gof] = fit(x, y, ft, fo);
+    params = zeros(length(start_in),1);
+    params(fix_ind) = start_in(fix_ind);
+    
+    p_err = ones(length(start_in),1);
+    p_err(fix_ind) = 0;
+
+    chisq = sum(((feval(fitobject, x) - y)./err).^2)/(length(x)-length(start)-1);
+    params(params == 0) = coeffvalues(fitobject);
+    p_err(p_err == 1) = mean(abs(confint(fitobject) - [coeffvalues(fitobject); coeffvalues(fitobject)]), 1);
+
+
+end
+
