1f736a117ff9a57ad48d30baf2e4aedeebf90b6f
Basti <pfitzseb@physik.hu-berlin.de>
2015-02-05 14:43:22 +0100

    deployment angepasst
    Former-commit-id: e7891f18611900b26037c53271940e6df4914917

--- Deployment/deploy.m
+++ Deployment/deploy.m
@@ -1,50 +1,57 @@
-%% read new version from source file and check against latest online version
+%% config
+% change these four rows to suit your application
 prjct = 'sisa-scan-auswertung';
 path_to_prjct = 'C:\Users\pfitzseb\Dokumente\Matlab\sisa-scan-auswertung\src';
 path_to_binary_versions = 'C:\Users\pfitzseb\Dokumente\Git\binary_versions';
+local_version = get_local_version(path_to_prjct);
 
-% get version
-addpath(path_to_prjct);
-strct = readini(fullfile(path_to_prjct, 'config.ini'));
-version = strct.version;
-version = regexprep(version, '0+$', ''); % strip zeroes at the end
-rmpath(path_to_prjct);
 
-online_version = str2double(urlread('http://git.daten.tk/sebastian.pfitzner/binary_versions/raw/master/sisa-scan-auswertung.ver'));
+version_url = ['http://git.daten.tk/sebastian.pfitzner/binary_versions/raw/master/' prjct '.ver'];
 
+%% read new version from source file and check against latest online version
 build = true;
 newver = true;
-if online_version >= str2double(version)
+
+online_version = str2double(urlread(version_url));
+
+if online_version >= str2double(local_version)
     newver = false;
-    warning(['Online version number (' num2str(online_version)...
-           ') is equal to the local number version (' version ').']);
+    warning(['Local version (' local_version ') is NOT greater than'...
+             'online version (' num2str(online_version) ').']);
     build = input('Build anyway? (0|1) ');
 end
 
 %% compile the binary
 if build
     mcc -e  -o SiSaScanAuswertung -d SiSaScanAuswertung/standalone ../src/startUI.m
+    if ~newver
+        warning('Will not push the new version and will not update the version number.');
+    end
 end
 
 %% push the new version number to binary_versions repo
 if newver
-    str = ['cd ' path_to_binary_versions ' & git pull & @echo ' num2str(version)...
-           '> ' prjct '.ver & git add ' prjct '.ver & git commit -m "tagged version ' version ' of '...
-           prjct '" & git push origin master'];
+    str = ['cd ' path_to_binary_versions ' && git pull && @echo ' num2str(local_version)...
+           '> ' prjct '.ver && git add ' prjct '.ver && git commit -m "tagged version ' local_version ' of '...
+           prjct '" && git push origin master'];
 
     done = system(str);
     if done == 0
+        sprintf('\n ----- \n');
         disp('Successfully pushed the new version number to GitLab!');
+        sprintf('\n ----- \n');
     end
 end
 
 %% push the binaries to your online repo
 if newver
-    str = ['cd ' path_to_prjct ' & git pull origin master & git add -u :/ & git commit -m "tagged version ' version ' of '...
-           prjct '" & git push origin master'];
+    str = ['cd ' path_to_prjct ' && git pull origin master && git add -u :/ && git commit -m "tagged version ' local_version ' of '...
+           prjct '" && git push origin master'];
 
     done = system(str);
     if done == 0
-        disp('Successfully pushed the new version to GitLab!');
+        sprintf('\n ----- \n');
+        disp('Successfully pushed the new version''s binaries to GitLab!');
+        sprintf('\n ----- \n');
     end
 end
---
+++ Deployment/get_local_version.m
@@ -0,0 +1,8 @@
+function [ version ] = get_local_version(path_to_prjct)
+    addpath(path_to_prjct);
+    strct = readini(fullfile(path_to_prjct, 'config.ini'));
+    version = strct.version;
+    version = regexprep(version, '0+$', ''); % strip zeroes at the end
+    rmpath(path_to_prjct);
+end
+
