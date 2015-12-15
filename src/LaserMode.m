8866a48ecf2aa6d097def785a6ccc4069f41232f
Basti <pfitzseb@physik.hu-berlin.de>
2015-12-15 17:02:01 +0100

    lasermode aktualisiert und tatsächlich hinzugefügt
    Former-commit-id: 344d30788cfbc36fd31f54c11beb5f154fe91219 [formerly 344d30788cfbc36fd31f54c11beb5f154fe91219 [formerly 344d30788cfbc36fd31f54c11beb5f154fe91219 [formerly da23c37c3d5bf527e22bbb53ecafd0baa39caa09]]]
    Former-commit-id: e85f57cd153ed355dc328a9fd756a828bc919ce7
    Former-commit-id: aa94d01146d3bc79c7422112b0a96abfe8953b23
    Former-commit-id: b9205ff090cc5a4def985e9e89aadc955db05d12
    Former-commit-id: 9028de26877ab72abaacd5b53a2d8f1990266daf

---
+++ src/LaserMode.m
@@ -0,0 +1,66 @@
+classdef LaserMode < GenericMode
+    %LASERMODE
+    % Platzhalter. Macht was draus! :)
+    
+    methods
+        function this = LaserMode(parent, data)
+            this.p = parent;
+            data(data == 0) = nan;
+            this.data = data;
+            
+            this.h.parent = parent.h.modepanel;
+            
+            this.h.lasermode = uitab(this.h.parent);
+                             
+            this.h.plotpanel = uipanel(this.h.lasermode);
+            
+            set(this.h.lasermode, 'title', 'Laser-Leistung',...
+                                 'tag', '4');
+                             
+            %% Plot
+            set(this.h.plotpanel, 'units', 'pixels',...
+                                'position', [5 5 500 500],...
+                                'bordertype', 'line',...
+                                'highlightcolor', [.7 .7 .7],...
+                                'BackgroundColor', [.85 .85 .85]);
+            
+            dims = size(data);
+            this.plotpanel = PlotPanel(this, dims, {'x', 'y', 'z', 's'}, this.h.plotpanel);
+
+            this.resize();
+            this.plot_array();
+        end
+                
+        function save_fig(this, varargin)
+            np = this.plotpanel.save_fig([this.p.savepath filesep() this.p.genericname...
+                                          '_Temp.pdf']);
+            this.p.set_savepath(np);
+        end     
+        
+        function plot_array(this)
+            this.plotpanel.plot_array(this.data, 'm1');
+        end
+        
+        function right_click_on_axes(this, point)
+            % dummy against errors
+        end
+        
+        function left_click_on_axes(this, point)
+            % dummy against errors
+        end
+        
+        function resize(this, varargin)
+            mP = get(this.h.parent, 'position');
+            mP(4) = mP(4) - 25;
+            
+            pP = get(this.h.plotpanel, 'Position');
+            pP(3:4) = [(mP(3)-pP(1))-10 (mP(4)-pP(2))-10];
+            set(this.h.plotpanel, 'Position', pP);
+        end
+        
+        function destroy(this, children_only)
+            delete(this);
+        end
+    end
+end
+
