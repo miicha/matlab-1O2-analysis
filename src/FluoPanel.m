121d24f9751ddc152068488ae15456001412f907
Basti <pfitzseb@physik.hu-berlin.de>
2016-01-11 19:18:27 +0100

    fix für wellenlängenauswahl im fluomode
    Former-commit-id: ae6e156d74837193c15e788f0316c5e72551734e [formerly ae6e156d74837193c15e788f0316c5e72551734e [formerly ae6e156d74837193c15e788f0316c5e72551734e [formerly 06227550a09cd1f72ffa350fc4b341d361664cb3]]]
    Former-commit-id: 3b62630f0f9e4a2f91d1a36cbfc68caa934690e5
    Former-commit-id: dd22711dfcc68b0e2175fcb5eade61c5f295bbb4
    Former-commit-id: 169120f3a04d35435ba559817e8cd41665d55ca0
    Former-commit-id: 34d27f4e94302307df61dd70cc7a9cbc0b23a382

--- src/FluoPanel.m
+++ src/FluoPanel.m
@@ -3,27 +3,32 @@ classdef FluoPanel < PlotPanel
     methods
         function this = FluoPanel(parent, dims, gui_parent)
             dimnames = {'x', 'y', 'z', 's', 'l'};
-            this@PlotPanel(parent, dims, dimnames, gui_parent, dimnames ,{[], [], [], [], []});
-            
-            this.h.d5_slider.Callback = @this.set_wl_cb;
-            this.h.d5_edit.Callback = @this.set_wl_cb;          
+            this@PlotPanel(parent, dims, dimnames, gui_parent, dimnames ,{[], [], [], [], []});       
         end
         
-        function set_wl_cb(this, caller, varargin)
-            dim = 5;
-            switch caller.Style
-                case 'slider'
-                    value = round(caller.Value);
-                case 'edit' 
-                    value = round(str2double(caller.String));
-                    [~, value] = min(abs(this.p.wavelengths - value));
+    end
+    
+    methods (Access = protected)      
+        function set_nth_val_cb(this, caller, varargin)
+            dim = str2double(caller.Tag);
+            if strcmp(this.dimnames{this.curr_dims(dim)}, 'l')
+                switch caller.Style
+                    case 'slider'
+                        value = round(caller.Value);
+                    case 'edit' 
+                        value = round(str2double(caller.String));
+                        [~, value] = min(abs(this.p.wavelengths - value));
+                end
+
+                value = this.set_nth_val(dim, value);
+
+                this.h.(sprintf('d%d_edit', dim)).String = sprintf('%.1f', this.p.wavelengths(value));
+                this.h.(sprintf('d%d_slider', dim)).Value = value;
+            else
+                set_nth_val_cb@PlotPanel(this, caller, varargin{:})
             end
-            
-            value = this.set_nth_val(dim, value);
-            
-            this.h.(sprintf('d%d_edit', dim)).String = sprintf('%.1f', this.p.wavelengths(value));
-            this.h.(sprintf('d%d_slider', dim)).Value = value;
         end
+            
     end
     
 end
