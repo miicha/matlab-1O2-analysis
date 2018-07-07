727a70f9aaa43e84c85c2c550e47e8abf5063921
Michael Pfitzner <michael.pfitzner@physik.hu-berlin.de>
2018-07-07 14:40:42 +0200

    globale startwerte auch für punktplot übernehmen

--- src/SiSaPointPlot.m
+++ src/SiSaPointPlot.m
@@ -58,6 +58,9 @@ classdef SiSaPointPlot < SiSaGenericPlot
             par_names = this.sisa_fit.parnames;
             for i = 1:this.sisa_fit.par_num
                 set(this.h.pc{i}, 'Value', ismember(par_names(i), this.smode.fix));
+                if this.smode.use_gstart(i) == 1
+                    this.h.pe{i}.String = num2str(this.smode.gstart(i));
+                end
             end
         end
         
