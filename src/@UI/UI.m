4776d314e98a1b7b16a0ef00f3765bee597eb368
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-08-03 16:34:07 +0200

    tagged version 0.6.1 of sisa-scan-auswertung

--- src/@UI/UI.m
+++ src/@UI/UI.m
@@ -7,7 +7,7 @@ classdef UI < handle
     end
     
     properties
-        version = '0.6.0';
+        version = '0.6.1';
         lastopened = 0;
         fileinfo = struct('path', '', 'size', [0 0 0 0],...
                           'name', '', 'np', 0); 
